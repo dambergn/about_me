d803afc4bc05ccb74bce5a58ea6daa051b5f9081
dambergn <dambergn@gmail.com>
2017-09-28 00:20:52 -0700

    class changes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+'use strict'; //follow exactly
 
+var userPoints = 0;
+
+console.log('hello world');
+alert('Welcome to my gussing game!');
+
+var user = prompt('What is your name?');
+alert('Hi ' + user + '!  I am so glad you want to play my game.');
+
+var answer = prompt('is Nick\'s favorite food sushi?  Type yes or no.').toLowerCase();
+console.log(answer);
+
+if(answer === 'yes' || answer === 'Y'){
+  userPoints++;
+}else{
+  alert('Wrong!');
+}
+alert('you have ' + userPoints + ' points.');
