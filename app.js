00e4dc316c58b4e6c18b44a0fc233fa2ab663c71
dambergn <dambergn@gmail.com>
2017-09-30 09:53:08 -0700

    fixed consol log messages.

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 'use strict'; //follow exactly
+
+//Custom alert window
+function CustomAlert(){
+  this.render = function(dialog){
+    var winW = window.innerWidth;
+    var winH = window.innerHeight;
+    var dialogoverlay = document.getElementById('dialogoverlay');
+    var dialogbox = document.getElementById('dialogbox');
+    dialogoverlay.style.display = "block";
+    dialogoverlay.style.height = winH+"px";
+    dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+    dialogbox.style.top = "100px";
+    dialogbox.style.display = "block";
+    document.getElementById('dialogboxhead').innerHTML = "Your results";
+    document.getElementById('dialogboxbody').innerHTML = dialog;
+    document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+  }
+  this.ok = function(){
+    document.getElementById('dialogbox').style.display = "none";
+    document.getElementById('dialogoverlay').style.display = "none";
+  }
+}
+//Notice Caps!
+var Alert = new CustomAlert();
+
+//Assignment essentials
 //Point counter
 var userPoints = 0;
 //Greeting
-console.log('hello world');
+console.log('Answers');
 alert('Welcome to my gussing game!');
 //Name Inquirery
 var user = prompt('What is your name?');
@@ -10,7 +36,7 @@ alert('Hi ' + user + '!  I am so glad you want to play my game.');
 
 //Question 1
 var answer1 = prompt('Is Nick\'s favorite food sushi?  Type yes or no.').toLowerCase();//makes answers all lowercase
-console.log(answer1);
+console.log('First answer, ' + answer1);
 //user answer input
 if(answer1 === 'yes' || answer1 === 'y'){
   userPoints++;
@@ -22,7 +48,7 @@ alert('you have ' + userPoints + ' points.');
 
 //Question 2
 var answer2 = prompt('Is Nick\'s favorite TV show Jersy Shore?  Type yes or no.').toLowerCase();//makes answers all lowercase
-console.log(answer2);
+console.log('Second answer, ' + answer2);
 //user answer input
 if(answer2 === 'no' || answer2 === 'n'){
   userPoints++;
@@ -34,7 +60,7 @@ alert('you have ' + userPoints + ' points.');
 
 //Question 3
 var answer3 = prompt('Does Nick like working with his hands?  Type yes or no.').toLowerCase();//makes answers all lowercase
-console.log(answer2);
+console.log('Third answer, ' + answer3);
 //user answer input
 if(answer3 === 'yes' || answer3 === 'y'){
   userPoints++;
@@ -46,7 +72,7 @@ alert('you have ' + userPoints + ' points.');
 
 //Question 4
 var answer4 = prompt('Does Nick spend to much time in front of the computer?  Type yes or no.').toLowerCase();//makes answers all lowercase
-console.log(answer2);
+console.log('Fourth answer, ' + answer4);
 //user answer input
 if(answer4 === 'yes' || answer4 === 'y'){
   userPoints++;
@@ -58,7 +84,7 @@ alert('you have ' + userPoints + ' points.');
 
 //Question 5
 var answer5 = prompt('Does Nick play to many video games?  Type yes or no.').toLowerCase();//makes answers all lowercase
-console.log(answer2);
+console.log('Fifth answer, ' + answer5);
 //user answer input
 if(answer5 === 'no' || answer5 === 'n'){
   userPoints++;
@@ -66,4 +92,4 @@ if(answer5 === 'no' || answer5 === 'n'){
   alert('No, he rarely has time to play games anymore. It\'s very sad.');
 }
 //Points update
-alert('you have ' + userPoints + ' points.');
+alert('you now have ' + userPoints + ' points.');
